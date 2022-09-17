31d0594b690153e2652f230677ac99d67bfd0a48
Matt Mauler <matt@metasonic.studio>
2022-09-17 14:32:29 +0200

    Initial Commit

---
+++ SplitIt/SplitItApp.swift
@@ -0,0 +1,17 @@
+//
+//  SplitItApp.swift
+//  SplitIt
+//
+//  Created by Matěj Mauler on 17.09.2022.
+//
+
+import SwiftUI
+
+@main
+struct SplitItApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
